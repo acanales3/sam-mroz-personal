35dc179ce5224f608cfa66556e148afbeaf53793
acanales3 <alexander.s.canales@gmail.com>
2024-04-03 19:41:21 -0500

    side tabs on nav

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
-import { FaBars, FaTimes } from "react-icons/fa";
+import { FaBars, FaGithub, FaLinkedin, FaTimes } from "react-icons/fa";
+import {
+  BsFillPersonLinesFill,
+  BsReverseLayoutTextSidebarReverse,
+} from "react-icons/bs";
 import { Separator } from "./ui/separator";
 
 function Nav() {
@@ -9,8 +13,8 @@ function Nav() {
   const refreshClick = () => setToggle(!toggle);
 
   return (
-    <nav className="sticky top-0 z-10 flex h-20 w-full items-center border-b border-primary bg-zinc-50 px-4 text-xl font-semibold md:justify-center">
-      <ul className="hidden items-center gap-4 md:flex">
+    <nav className="sticky top-0 z-10 flex h-20 w-full items-center border-b border-primary bg-zinc-50 px-4 text-xl md:justify-center">
+      <ul className="hidden items-center gap-4 font-semibold md:flex">
         <li>
           <a href="/" className="decoration-primary hover:underline">
             About Me
@@ -53,11 +57,6 @@ function Nav() {
         >
           <a href="/">
             <li className="py-6 text-4xl" onClick={refreshClick}>
-              Home
-            </li>
-          </a>
-          <a href="/about">
-            <li className="py-6 text-4xl" onClick={refreshClick}>
               About Me
             </li>
           </a>
@@ -78,6 +77,38 @@ function Nav() {
           </a>
         </ul>
       </motion.div>
+      <div className="fixed left-0 top-[35%] hidden flex-col lg:flex">
+        <ul>
+          <li className="ml-[-100px] flex h-[60px] w-[160px] items-center justify-between rounded-xl bg-primary px-2 text-base duration-300 hover:ml-[-10px]">
+            <a
+              className="ml-[10px] flex w-full items-center justify-between text-zinc-50"
+              href="https://www.linkedin.com/in/samuel-mroz-458410219/"
+              target="_blank"
+            >
+              LinkedIn <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className="ml-[-100px] flex h-[60px] w-[160px] items-center justify-between rounded-xl bg-[#16b35a] px-2 text-base duration-300 hover:ml-[-10px]">
+            <a
+              className="ml-[10px] flex w-full items-center justify-between text-zinc-50"
+              href="Sam_Mroz_Resume.pdf"
+              download="sam-mroz-resume"
+            >
+              Download Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+          <li className="ml-[-100px] flex h-[60px] w-[160px] items-center justify-between rounded-xl bg-[#16c36a] px-2 text-base duration-300 hover:ml-[-10px]">
+            <a
+              className="ml-[10px] flex w-full items-center justify-between text-zinc-50"
+              href="Sam_Mroz_Unoffical_Transcript.pdf"
+              download="sam-mroz-transcript"
+            >
+              Download Transcript{" "}
+              <BsReverseLayoutTextSidebarReverse size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
